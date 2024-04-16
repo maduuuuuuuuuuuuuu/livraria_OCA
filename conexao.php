36eac0e2f4e57c81a0e94522403d3fb7696d2743
Maria Eduarda <e107ti@etec.sp.gov.br>
2024-04-15 22:40:18 -0300

    criação das telas: login, cadastro, tela principal e conexão com o banco de dados

---
+++ conexao.php
@@ -0,0 +1,10 @@
+<?php
+$servidor = "localhost";
+$user = "root";
+$password = "root";
+$banco = "bd_livraria";
+
+$conexao = new mysqli($servidor, $user, $password, $banco);
+
+
+?>
---
+++ insert_cliente.php
@@ -0,0 +1,21 @@
+<?php
+$nm_cliente = $_POST['nm_cliente'];
+$nm_endereco = $_POST['nm_endereco'];
+$nr_endereco = $_POST['nr_endereco'];
+$cpf = $_POST['cpf'];
+$cnpj = $_POST['cnpj'];
+$fone = $_POST['fone'];
+
+echo"$nm_cliente";
+echo"<br>";
+echo"$nm_endereco";
+echo"<br>";
+echo"$nr_endereco";
+echo"<br>";
+echo "$cpf";
+echo"<br>";
+echo"$cnpj";
+echo"<br>";
+echo"$fone";
+
+?>
