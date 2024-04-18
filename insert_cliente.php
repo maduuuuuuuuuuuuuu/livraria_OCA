be3d8714cbbf2a1c2145d65aef05d70b513a1a63
Maria Eduarda <e107ti@etec.sp.gov.br>
2024-04-17 22:07:12 -0300

    back finalizado

--- insert_cliente.php
+++ insert_cliente.php
@@ -6,16 +6,13 @@ $cpf = $_POST['cpf'];
 $cnpj = $_POST['cnpj'];
 $fone = $_POST['fone'];
 
-echo"$nm_cliente";
-echo"<br>";
-echo"$nm_endereco";
-echo"<br>";
-echo"$nr_endereco";
-echo"<br>";
-echo "$cpf";
-echo"<br>";
-echo"$cnpj";
-echo"<br>";
-echo"$fone";
+include 'conexao.php';
+
+$insert = "INSERT INTO tb_cliente VALUES (NULL, '$nm_cliente', '$nm_endereco', '$nr_endereco', '$cpf', '$cnpj', '$fone')";
+
+$query = mysqli_query($conexao, $insert);
+
+echo "Inserido com sucesso";
+?>
 
 ?>
