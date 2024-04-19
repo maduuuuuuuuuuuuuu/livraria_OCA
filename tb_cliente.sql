911dc485ce68beeeffe6afab1ccb28a35f54f8c9
Maria Eduarda <e107ti@etec.sp.gov.br>
2024-04-18 22:39:16 -0300

    Finalizado!

---
+++ tb_cliente.sql
@@ -0,0 +1,71 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Tempo de geração: 19-Abr-2024 às 03:37
+-- Versão do servidor: 8.0.27
+-- versão do PHP: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `bd_livraria`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tb_cliente`
+--
+
+CREATE TABLE `tb_cliente` (
+  `id_cliente` int NOT NULL,
+  `nm_cliente` varchar(45) COLLATE utf8mb4_general_ci NOT NULL,
+  `nm_endereco` varchar(45) COLLATE utf8mb4_general_ci NOT NULL,
+  `nr_endereco` int NOT NULL,
+  `nr_cpf` char(11) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `nr_cnpj` char(14) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `nr_fone` varchar(11) COLLATE utf8mb4_general_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Extraindo dados da tabela `tb_cliente`
+--
+
+INSERT INTO `tb_cliente` (`id_cliente`, `nm_cliente`, `nm_endereco`, `nr_endereco`, `nr_cpf`, `nr_cnpj`, `nr_fone`) VALUES
+(1, 'Melissa dos Santos Portela', 'Rua Horácio Martins Domingues', 457, '12345678901', '', '13543768905'),
+(2, 'Melissa dos Santos Portela', 'Rua Horácio Martins Domingues', 457, '12345678901', '', '13543768905');
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices para tabela `tb_cliente`
+--
+ALTER TABLE `tb_cliente`
+  ADD PRIMARY KEY (`id_cliente`);
+
+--
+-- AUTO_INCREMENT de tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `tb_cliente`
+--
+ALTER TABLE `tb_cliente`
+  MODIFY `id_cliente` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
